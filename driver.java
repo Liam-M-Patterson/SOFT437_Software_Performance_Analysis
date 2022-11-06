21ae6ad72493505dfbfb30ba536791bad238c748
liampatterson02@gmail.com <liampatterson02@gmail.com>
2022-11-06 16:19:42 -0500

    changed sorting algorithms to be their own class

--- driver.java
+++ driver.java
@@ -2,82 +2,6 @@ import java.util.Random;
 
 public class driver {
     
-    // A utility function to swap two elements
-    static void swap(int[] arr, int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-  
-    /* This function takes last element as pivot, places
-       the pivot element at its correct position in sorted
-       array, and places all smaller (smaller than pivot)
-       to left of pivot and all greater elements to right
-       of pivot */
-    static int partition(int[] arr, int low, int high)
-    {
-  
-        // pivot
-        int pivot = arr[high];
-  
-        // Index of smaller element and
-        // indicates the right position
-        // of pivot found so far
-        int i = (low - 1);
-  
-        for (int j = low; j <= high - 1; j++) {
-  
-            // If current element is smaller than the pivot
-            if (arr[j] < pivot) {
-  
-                // Increment index of smaller element
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, high);
-        return (i + 1);
-    }
-  
-
-    static void quickSort(int[] arr) {
-        quickSort(arr, 0, arr.length-1);
-    }
-
-    /* The main function that implements QuickSort
-        arr[] --> Array to be sorted,
-        low --> Starting index,
-        high --> Ending index
-    */
-    static void quickSort(int[] arr, int low, int high)
-    {
-        if (low < high) {
-  
-            // pi is partitioning index, arr[p]
-            // is now at right place
-            int pi = partition(arr, low, high);
-  
-            // Separately sort elements before
-            // partition and after partition
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
-    }
-
-    static void bubbleSort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (arr[j] > arr[j + 1]) {
-                    // swap arr[j+1] and arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-    }
-
     static int[] populateArray(int length, int max){
         Random rand = new Random();
 
@@ -118,11 +42,11 @@ public class driver {
         ins.stopTiming(null);
 
         ins.startTiming("QuickSort");
-        quickSort(arr);
+        QuickSort.quickSort(arr);
         ins.stopTiming("Done sorting");
 
         ins.startTiming("BubbleSort");
-        bubbleSort(arr);
+        BubbleSort.bubbleSort(arr);
         ins.stopTiming("Done sorting");
 
         ins.startTiming("Printing");
